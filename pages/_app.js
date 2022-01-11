296a620b9eb9262c90f685c7b8f751dea7596311
saladinjake <juwavictor@gmail.com>
2022-01-11 14:55:04 +0100

    injecting chakra theme

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,15 @@
 import "../core/assets/styles/globals.css"
 import "../core/assets/styles/main.scss"
 import React from "react"
+import { ChakraProvider } from "@chakra-ui/react";
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+    )
   }
+
+
+
+
