d99db5cc73d026b78567cb387326c49b0cf2fedd
saladinjake <juwavictor@gmail.com>
2022-05-15 00:00:00 +0100

    feat(auth): modernize signin page with premium split-screen and glassmorphic layout

--- pages/signin.js
+++ pages/signin.js
@@ -16,9 +16,11 @@ import {
   InputLeftElement,
   VStack,
   ScaleFade,
+  HStack,
+  Badge,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import { HiMail, HiLockClosed } from "react-icons/hi";
+import { HiMail, HiLockClosed, HiArrowRight, HiSparkles } from "react-icons/hi";
 import Link from "next/link";
 import Layout from "components/shared/blocks/Layout";
 import http from "utils/http";
@@ -32,15 +34,15 @@ const MotionFlex = motion(Flex);
 const initialFieldsProps = [
   {
     id: "email",
-    label: "Email",
-    placeholder: "Email address",
+    label: "Email Address",
+    placeholder: "e.g. john@example.com",
     type: "email",
     icon: HiMail,
   },
   {
     id: "password",
     label: "Password",
-    placeholder: "Password",
+    placeholder: "••••••••",
     type: "password",
     icon: HiLockClosed,
   },
@@ -60,15 +62,15 @@ const SignInPage = () => {
 
       cookie.setToken(token);
       toast.displayToast({
-        title: "Success",
-        description: "You've successfully signed in.",
+        title: "Welcome back!",
+        description: "You've successfully signed in to your account.",
         status: "success",
       });
       router.push("/signin/auth-check");
     } catch (err) {
       toast.displayToast({
-        title: "Error",
-        description: err.response?.data?.message || "Invalid credentials",
+        title: "Sign in failed",
+        description: err.response?.data?.message || "Please check your credentials and try again.",
         status: "error",
       });
     }
@@ -76,58 +78,84 @@ const SignInPage = () => {
 
   const form = useForm({ doSubmit, initialFieldsProps });
 
+  const bgColor = useColorModeValue("gray.50", "gray.900");
+  const cardBg = useColorModeValue("white", "gray.800");
+
   return (
     <Layout>
-      <Flex minH={"100vh"} align={"center"} justify={"center"} bg={useColorModeValue("gray.50", "gray.800")}>
-        <Stack spacing={0} mx={"auto"} maxW={"lg"} py={12} px={6} direction={{ base: "column", md: "row" }} w="full" rounded="3xl" overflow="hidden" shadow="2xl">
-          {/* Image Side */}
+      <Flex minH={"100vh"} align={"center"} justify={"center"} bg={bgColor} p={4}>
+        <Stack
+          spacing={0}
+          mx={"auto"}
+          maxW={"6xl"}
+          w="full"
+          direction={{ base: "column", md: "row" }}
+          rounded="3xl"
+          overflow="hidden"
+          shadow="2xl"
+          bg={cardBg}
+          border="1px solid"
+          borderColor={useColorModeValue("gray.100", "gray.700")}
+        >
+          {/* Left: Branding & Content */}
           <Box
             display={{ base: "none", md: "block" }}
-            flex={1}
-            bgImage="url('/img/auth-bg.png')"
+            flex={1.2}
+            bgImage="url('/img/herolanding.jpg')"
             bgSize="cover"
             bgPosition="center"
             position="relative"
           >
+            <Box position="absolute" inset={0} bgGradient="linear(to-br, blue.600, blue.900)" opacity={0.85} />
             <Flex
-              pos="absolute"
-              inset={0}
-              bg="blackAlpha.400"
+              pos="relative"
+              h="full"
               direction="column"
-              justify="center"
+              justify="space-between"
               p={12}
               color="white"
             >
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8 }}
-              >
-                <Heading size="2xl" mb={4}>Welcome Back</Heading>
-                <Text fontSize="xl" opacity={0.9}>
-                  Experience learning from a different perspective.
+              <VStack align="start" spacing={2}>
+                <HStack color="blue.300">
+                  <Icon as={HiSparkles} />
+                  <Text fontWeight="black" letterSpacing="widest" fontSize="xs">PREMIUM ACCESS</Text>
+                </HStack>
+                <Heading size="3xl" fontWeight="black" letterSpacing="tight">
+                  Start Your <Text as="span" color="blue.300">Transformation</Text>
+                </Heading>
+              </VStack>
+
+              <VStack align="start" spacing={6}>
+                <Text fontSize="xl" fontWeight="medium" opacity={0.9}>
+                  Join over 1 million learners and professionals who are shaping the future with our advanced AI-driven platform.
                 </Text>
-              </motion.div>
+                <HStack spacing={4}>
+                  <Badge bg="whiteAlpha.300" color="white" px={3} py={1} rounded="full">LMS</Badge>
+                  <Badge bg="whiteAlpha.300" color="white" px={3} py={1} rounded="full">Analytics</Badge>
+                  <Badge bg="whiteAlpha.300" color="white" px={3} py={1} rounded="full">Certification</Badge>
+                </HStack>
+              </VStack>
             </Flex>
           </Box>
 
-          {/* Form Side */}
+          {/* Right: Form */}
           <Box
             flex={1}
-            bg={useColorModeValue("white", "gray.700")}
-            p={{ base: 8, md: 12 }}
+            p={{ base: 8, md: 16 }}
             position="relative"
+            bg={cardBg}
           >
-            <ScaleFade initialScale={0.9} in={true}>
-              <Stack spacing={8}>
-                <Stack align={"center"}>
-                  <Heading fontSize={"3xl"} textAlign={"center"}>
-                    Sign in to your account
+            <ScaleFade initialScale={0.95} in={true}>
+              <VStack spacing={10} align="stretch">
+                <VStack align="start" spacing={2}>
+                  <Heading size="xl" fontWeight="black" letterSpacing="tight">
+                    Welcome Back
                   </Heading>
-                  <Text fontSize={"lg"} color={"gray.600"}>
-                    to enjoy all of our cool <ChakraLink color={"blue.400"}>features</ChakraLink> ✌️
+                  <Text color={"gray.500"} fontSize="lg">
+                    Enter your credentials to access your dashboard
                   </Text>
-                </Stack>
+                </VStack>
+
                 <Box
                   as="form"
                   onSubmit={(e) => {
@@ -135,24 +163,26 @@ const SignInPage = () => {
                     form.handleSubmit(e);
                   }}
                 >
-                  <Stack spacing={4}>
+                  <Stack spacing={6}>
                     {initialFieldsProps.map((field) => (
-                      <VStack align="start" key={field.id} spacing={1}>
-                        <Text fontWeight="medium" fontSize="sm">{field.label}</Text>
-                        <InputGroup>
+                      <VStack align="start" key={field.id} spacing={2}>
+                        <Text fontWeight="bold" fontSize="sm" color="gray.600">{field.label}</Text>
+                        <InputGroup size="lg">
                           <InputLeftElement pointerEvents="none">
-                            <Icon as={field.icon} color="gray.400" />
+                            <Icon as={field.icon} color="blue.400" />
                           </InputLeftElement>
                           <Input
                             type={field.type}
                             placeholder={field.placeholder}
-                            rounded="lg"
-                            bg={useColorModeValue("gray.50", "gray.800")}
-                            border={0}
+                            rounded="2xl"
+                            bg={useColorModeValue("gray.50", "gray.900")}
+                            borderWidth="2px"
+                            borderColor="transparent"
+                            fontSize="md"
                             _focus={{
-                              bg: "white",
-                              ring: 2,
-                              ringColor: "blue.400"
+                              bg: useColorModeValue("white", "gray.800"),
+                              borderColor: "blue.400",
+                              shadow: "lg",
                             }}
                             name={field.id}
                             onChange={form.handleType}
@@ -161,47 +191,59 @@ const SignInPage = () => {
                       </VStack>
                     ))}
 
-                    <Stack spacing={10}>
-                      <Stack
-                        direction={{ base: "column", sm: "row" }}
-                        align={"start"}
-                        justify={"space-between"}
-                      >
-                        <Checkbox>Remember me</Checkbox>
-                        <Link href="/forgot-password" passHref>
-                          <ChakraLink color={"blue.400"}>Forgot password?</ChakraLink>
-                        </Link>
-                      </Stack>
-                      <Button
-                        type="submit"
-                        bg={"blue.400"}
-                        color={"white"}
-                        rounded={"lg"}
-                        _hover={{
-                          bg: "blue.500",
-                          transform: "translateY(-2px)",
-                          boxShadow: "lg",
-                        }}
-                        _active={{
-                          bg: "blue.600",
-                        }}
-                        isLoading={form.formSubmitState.isSubmitting}
-                        transition="all 0.2s"
-                      >
-                        Sign in
-                      </Button>
-                    </Stack>
-                    <Stack pt={6}>
-                      <Text align={"center"}>
-                        Don't have an account?{" "}
+                    <HStack justify="space-between" pt={2}>
+                      <Checkbox colorScheme="blue" defaultChecked>
+                        <Text fontSize="sm" color="gray.500">Remember me</Text>
+                      </Checkbox>
+                      <Link href="/forgot-password" passHref>
+                        <ChakraLink color={"blue.500"} fontSize="sm" fontWeight="bold">
+                          Forgot password?
+                        </ChakraLink>
+                      </Link>
+                    </HStack>
+
+                    <Button
+                      type="submit"
+                      size="lg"
+                      colorScheme="blue"
+                      h={16}
+                      rounded="2xl"
+                      fontSize="lg"
+                      fontWeight="bold"
+                      shadow="xl"
+                      rightIcon={<HiArrowRight />}
+                      _hover={{
+                        transform: "translateY(-2px)",
+                        shadow: "2xl",
+                      }}
+                      _active={{
+                        transform: "translateY(0)",
+                      }}
+                      isLoading={form.formSubmitState.isSubmitting}
+                      transition="all 0.2s"
+                    >
+                      Sign In
+                    </Button>
+
+                    <Flex align="center" pt={4}>
+                      <Divider />
+                      <Text px={4} color="gray.400" fontSize="xs" fontWeight="bold" textTransform="uppercase">OR</Text>
+                      <Divider />
+                    </Flex>
+
+                    <VStack spacing={4}>
+                      <Text align={"center"} color="gray.500">
+                        New to the platform?{" "}
                         <Link href="/signup" passHref>
-                          <ChakraLink color={"blue.400"}>Sign up</ChakraLink>
+                          <ChakraLink color={"blue.500"} fontWeight="black">
+                            Create an account
+                          </ChakraLink>
                         </Link>
                       </Text>
-                    </Stack>
+                    </VStack>
                   </Stack>
                 </Box>
-              </Stack>
+              </VStack>
             </ScaleFade>
           </Box>
         </Stack>
@@ -211,4 +253,5 @@ const SignInPage = () => {
 };
 
 export default SignInPage;
+
 
