666e7fc5f751cf40770c9cb6d5094b232aa76ef8
saladinjake <juwavictor@gmail.com>
2022-05-15 08:00:00 +0100

    feat(checkout): implement premium multi-step wizard with real-time order summary

--- pages/checkout.js
+++ pages/checkout.js
@@ -16,6 +16,7 @@ import {
   Divider,
   Circle,
   ScaleFade,
+  Badge,
 } from "@chakra-ui/react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
@@ -25,6 +26,7 @@ import {
   HiCreditCard,
   HiChevronRight,
   HiChevronLeft,
+  HiLockClosed,
 } from "react-icons/hi";
 import { Layout, Section, Loader, Empty } from "components/components/pages";
 import { Image, TextField } from "components/shared/lib";
@@ -34,7 +36,7 @@ import http from "utils/http";
 import formatPrice from "utils/formatPrice";
 import buildSEO from "utils/buildSEO";
 
-const pageSEO = buildSEO("Checkout", "Complete your purchase securely");
+const pageSEO = buildSEO("Secure Checkout", "Complete your premium purchase securely and efficiently");
 
 const steps = [
   { label: "Review Cart", icon: HiShoppingCart },
@@ -44,31 +46,40 @@ const steps = [
 
 const StepIndicator = ({ activeStep }) => {
   return (
-    <HStack spacing={4} justify="center" mb={12}>
+    <HStack spacing={0} justify="center" mb={16} position="relative" w="full" maxW="3xl" mx="auto">
       {steps.map((step, idx) => (
         <React.Fragment key={idx}>
-          <VStack spacing={2} align="center">
+          <VStack spacing={3} align="center" position="relative" zIndex={1} flex={1}>
             <Circle
-              size="12"
-              bg={idx <= activeStep ? "blue.500" : "gray.200"}
+              size="14"
+              bg={idx < activeStep ? "green.500" : idx === activeStep ? "blue.600" : "gray.100"}
               color={idx <= activeStep ? "white" : "gray.400"}
-              shadow={idx === activeStep ? "lg" : "none"}
-              transition="all 0.3s"
+              shadow={idx === activeStep ? "xl" : "none"}
+              transition="all 0.4s cubic-bezier(.175,.885,.32,1.275)"
+              border="4px solid"
+              borderColor={idx === activeStep ? "blue.50" : "transparent"}
             >
               <Icon as={idx < activeStep ? HiCheck : step.icon} w={6} h={6} />
             </Circle>
             <Text
               fontSize="xs"
-              fontWeight="bold"
-              color={idx <= activeStep ? "gray.700" : "gray.400"}
+              fontWeight="black"
+              color={idx <= activeStep ? "blue.900" : "gray.400"}
               textTransform="uppercase"
-              letterSpacing="tighter"
+              letterSpacing="2px"
             >
               {step.label}
             </Text>
           </VStack>
           {idx < steps.length - 1 && (
-            <Box h="2px" w="12" bg={idx < activeStep ? "blue.500" : "gray.200"} mt="-6" transition="all 0.3s" />
+             <Box 
+               h="2px" 
+               flex={1} 
+               bg={idx < activeStep ? "green.500" : "gray.200"} 
+               mt="-8" 
+               mx="-4"
+               transition="all 0.4s" 
+             />
           )}
         </React.Fragment>
       ))}
@@ -77,17 +88,17 @@ const StepIndicator = ({ activeStep }) => {
 };
 
 const CartItem = ({ item }) => (
-  <Flex align="center" justify="space-between" py={4}>
-    <HStack spacing={4}>
-      <Box p={2} bg="white" rounded="lg" shadow="sm">
-        <Image w="60px" h="60px" src={item.imageUrl} isProduct objectFit="contain" />
+  <Flex align="center" justify="space-between" py={5} borderBottom="1px solid" borderColor="gray.50" _last={{ borderBottom: "none" }}>
+    <HStack spacing={5}>
+      <Box p={3} bg="gray.50" rounded="2xl" shadow="sm">
+        <Image w="70px" h="70px" src={item.imageUrl} isProduct objectFit="contain" />
       </Box>
       <VStack align="start" spacing={0}>
-        <Text fontWeight="bold">{item.title}</Text>
-        <Text fontSize="sm" color="gray.500">{item.qty} Piece{item.qty > 1 ? "s" : ""}</Text>
+        <Text fontWeight="black" fontSize="lg" letterSpacing="tight">{item.title}</Text>
+        <Badge colorScheme="blue" variant="subtle" rounded="full" px={2} fontSize="2xs">Qty: {item.qty}</Badge>
       </VStack>
     </HStack>
-    <Text fontWeight="bold">{formatPrice("en-NG", item.qtyPrice, "NGN")}</Text>
+    <Text fontWeight="black" fontSize="lg">{formatPrice("en-NG", item.qtyPrice, "NGN")}</Text>
   </Flex>
 );
 
@@ -100,10 +111,10 @@ const CheckoutPage = () => {
   const cardBg = useColorModeValue("white", "gray.800");
 
   const [formData, setFormData] = useState({
-    name: "Test User",
-    email: "user@example.com",
-    phone: "+234 800 000 000",
-    address: "123 Business St, Lagos",
+    name: "John Doe",
+    email: "john@example.com",
+    phone: "+234 812 345 6789",
+    address: "15 Admiralty Way, Lekki Phase 1",
     state: "Lagos",
   });
 
@@ -112,146 +123,183 @@ const CheckoutPage = () => {
 
   const handlePlaceOrder = async () => {
     try {
-      // Simulate order placement
       toast.displayToast({
-        title: "Order Received",
-        description: "Your order has been placed successfully!",
+        title: "Order Secured!",
+        description: "Your premium order has been processed successfully.",
         status: "success",
       });
       cart.resetCart();
       router.push("/store");
     } catch (err) {
-      toast.displayToast({ title: "Error", description: err.message, status: "error" });
+      toast.displayToast({ title: "Checkout Failed", description: err.message, status: "error" });
     }
   };
 
-  if (cart?.isEmpty) return <Layout SEO={pageSEO}><Container py={20}><Empty /></Container></Layout>;
+  if (cart?.isEmpty) return <Layout SEO={pageSEO}><Container py={32}><Empty title="Your cart is empty" description="Add some premium products to your cart before checking out." /></Container></Layout>;
 
   return (
     <Layout SEO={pageSEO} bg={bgColor}>
-      <Container maxW="container.xl" py={12}>
+      <Section pt={12} pb={24}>
         <StepIndicator activeStep={activeStep} />
 
-        <SimpleGrid columns={{ base: 1, lg: 3 }} spacing={10}>
+        <SimpleGrid columns={{ base: 1, lg: 3 }} spacing={12}>
           {/* Main Wizard */}
-          <GridItem colSpan={{ base: 1, lg: 2 }}>
+          <Box gridColumn={{ base: "span 1", lg: "span 2" }}>
             <AnimatePresence mode="wait">
               <motion.div
                 key={activeStep}
-                initial={{ opacity: 0, x: 20 }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: -20 }}
-                transition={{ duration: 0.3 }}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
               >
-                <Box bg={cardBg} p={8} rounded="2xl" shadow="sm" borderWidth="1px">
+                <Box bg={cardBg} p={{ base: 6, md: 12 }} rounded="3xl" shadow="2xl" borderWidth="1px" borderColor="gray.100">
                   {activeStep === 0 && (
-                    <VStack align="stretch" spacing={6}>
-                      <Heading size="md">Review Your Items</Heading>
+                    <VStack align="stretch" spacing={8}>
+                      <VStack align="start" spacing={1}>
+                        <Heading size="lg" fontWeight="black">Review Your Order</Heading>
+                        <Text color="gray.500">Confirm the items in your cart before proceeding</Text>
+                      </VStack>
                       <Divider />
-                      <Stack spacing={4}>
+                      <Stack spacing={2}>
                         {cart.data?.map((item) => <CartItem key={item.id} item={item} />)}
                       </Stack>
                     </VStack>
                   )}
 
                   {activeStep === 1 && (
-                    <VStack align="stretch" spacing={6}>
-                      <Heading size="md">Shipping Information</Heading>
+                    <VStack align="stretch" spacing={8}>
+                      <VStack align="start" spacing={1}>
+                        <Heading size="lg" fontWeight="black">Shipping Destination</Heading>
+                        <Text color="gray.500">Where should we deliver your premium selection?</Text>
+                      </VStack>
                       <Divider />
-                      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={6}>
-                        <TextField label="Full Name" value={formData.name} />
-                        <TextField label="Email" value={formData.email} />
-                        <TextField label="Phone" value={formData.phone} />
-                        <TextField label="Address" value={formData.address} />
-                        <TextField label="State" value={formData.state} />
+                      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={8}>
+                        <TextField label="Full Legal Name" value={formData.name} variant="filled" rounded="xl" />
+                        <TextField label="Contact Email" value={formData.email} variant="filled" rounded="xl" />
+                        <TextField label="Phone Number" value={formData.phone} variant="filled" rounded="xl" />
+                        <TextField label="Delivery Address" value={formData.address} variant="filled" rounded="xl" />
+                        <TextField label="State / Region" value={formData.state} variant="filled" rounded="xl" />
                       </SimpleGrid>
                     </VStack>
                   )}
 
                   {activeStep === 2 && (
-                    <VStack align="stretch" spacing={6}>
-                      <Heading size="md">Payment Method</Heading>
+                    <VStack align="stretch" spacing={8}>
+                      <VStack align="start" spacing={1}>
+                        <Heading size="lg" fontWeight="black">Secure Payment</Heading>
+                        <Text color="gray.500">Choose your preferred method of secure payment</Text>
+                      </VStack>
                       <Divider />
-                      <SimpleGrid columns={1} spacing={4}>
-                         <Box p={6} border="2px solid" borderColor="blue.500" bg="blue.50" rounded="xl" cursor="pointer">
-                            <HStack spacing={4}>
-                               <Icon as={HiCreditCard} w={8} h={8} color="blue.500" />
-                               <VStack align="start" spacing={0}>
-                                  <Text fontWeight="bold">Credit / Debit Card</Text>
-                                  <Text fontSize="sm" color="gray.500">Pay securely with your card via Paystack</Text>
+                      <Stack spacing={4}>
+                         <Box p={8} border="2px solid" borderColor="blue.500" bg="blue.50" rounded="2xl" cursor="pointer" shadow="lg" transition="all 0.2s" _hover={{ shadow: "xl" }}>
+                            <HStack spacing={6}>
+                               <Circle size={16} bg="white" shadow="md">
+                                  <Icon as={HiCreditCard} w={10} h={10} color="blue.500" />
+                               </Circle>
+                               <VStack align="start" spacing={1}>
+                                  <Text fontWeight="black" fontSize="xl">Pay via Paystack</Text>
+                                  <Text fontSize="md" color="blue.600">Securely pay with Credit/Debit Cards or Bank Transfer</Text>
                                </VStack>
+                               <Icon as={HiCheck} ml="auto" color="blue.500" w={8} h={8} />
                             </HStack>
                          </Box>
-                      </SimpleGrid>
+                      </Stack>
                     </VStack>
                   )}
 
-                  <Flex justify="space-between" mt={12}>
+                  <Flex justify="space-between" mt={16}>
                     <Button
+                      size="lg"
                       leftIcon={<HiChevronLeft />}
                       onClick={prevStep}
                       disabled={activeStep === 0}
                       variant="ghost"
+                      rounded="full"
+                      px={8}
                     >
                       Back
                     </Button>
                     {activeStep < steps.length - 1 ? (
                       <Button
+                        size="lg"
                         rightIcon={<HiChevronRight />}
                         onClick={nextStep}
                         colorScheme="blue"
-                        px={10}
+                        px={12}
+                        rounded="full"
+                        shadow="xl"
+                        _hover={{ transform: "translateY(-2px)", shadow: "2xl" }}
                       >
-                        Next Step
+                        Continue to {steps[activeStep + 1].label}
                       </Button>
                     ) : (
                       <Button
-                        colorScheme="green"
-                        px={10}
+                        colorScheme="blue"
+                        px={16}
+                        h={16}
                         onClick={handlePlaceOrder}
                         size="lg"
-                        shadow="xl"
-                        _hover={{ transform: "scale(1.05)" }}
+                        rounded="full"
+                        shadow="2xl"
+                        fontSize="xl"
+                        fontWeight="black"
+                        leftIcon={<HiLockClosed />}
+                        _hover={{ transform: "scale(1.02)", shadow: "dark-lg" }}
                       >
-                        Place Order
+                        Securely Place Order
                       </Button>
                     )}
                   </Flex>
                 </Box>
               </motion.div>
             </AnimatePresence>
-          </GridItem>
+          </Box>
 
           {/* Sidebar Summary */}
-          <GridItem>
-            <Box bg={cardBg} p={8} rounded="2xl" shadow="lg" position="sticky" top="100px">
-              <Heading size="md" mb={6}>Order Summary</Heading>
-              <VStack align="stretch" spacing={4}>
-                <Flex justify="space-between">
-                  <Text color="gray.500">Subtotal</Text>
-                  <Text fontWeight="bold">{formatPrice("en-NG", cart.total, "NGN")}</Text>
-                </Flex>
-                <Flex justify="space-between">
-                  <Text color="gray.500">Shipping</Text>
-                  <Text fontWeight="bold" color="green.500">FREE</Text>
-                </Flex>
-                <Divider />
-                <Flex justify="space-between" align="center">
-                  <Text fontSize="lg" fontWeight="bold">Total</Text>
-                  <Heading size="lg" color="blue.500">
-                    {formatPrice("en-NG", cart.total, "NGN")}
-                  </Heading>
-                </Flex>
-              </VStack>
-            </Box>
-          </GridItem>
+          <Box>
+            <VStack spacing={8} position="sticky" top="120px">
+               <Box bg={cardBg} p={10} rounded="3xl" shadow="2xl" w="full" borderWidth="1px" borderColor="gray.100">
+                  <Heading size="md" mb={8} fontWeight="black" letterSpacing="tight" textTransform="uppercase" fontSize="xs" color="gray.400">Order Summary</Heading>
+                  <VStack align="stretch" spacing={6}>
+                    <Flex justify="space-between">
+                      <Text color="gray.500" fontWeight="medium">Items Subtotal</Text>
+                      <Text fontWeight="black">{formatPrice("en-NG", cart.total, "NGN")}</Text>
+                    </Flex>
+                    <Flex justify="space-between">
+                      <Text color="gray.500" fontWeight="medium">Shipping & Handling</Text>
+                      <Text fontWeight="black" color="green.500">FREE</Text>
+                    </Flex>
+                    <Divider />
+                    <Flex justify="space-between" align="center">
+                      <VStack align="start" spacing={0}>
+                         <Text fontSize="md" fontWeight="black">Total Amount</Text>
+                         <Text fontSize="xs" color="gray.400">Including VAT</Text>
+                      </VStack>
+                      <Heading size="xl" color="blue.600" fontWeight="black" letterSpacing="tighter">
+                        {formatPrice("en-NG", cart.total, "NGN")}
+                      </Heading>
+                    </Flex>
+                  </VStack>
+               </Box>
+               
+               <VStack bg="blue.50" p={6} rounded="2xl" w="full" align="start" spacing={4}>
+                  <HStack color="blue.600">
+                     <Icon as={HiShieldCheck} w={6} h={6} />
+                     <Text fontWeight="bold" fontSize="sm">ImpactXplorer Guarantee</Text>
+                  </HStack>
+                  <Text fontSize="xs" color="blue.700">
+                     Your transaction is protected by 256-bit SSL encryption. All payments are processed securely by our trusted partners.
+                  </Text>
+               </VStack>
+            </VStack>
+          </Box>
         </SimpleGrid>
-      </Container>
+      </Section>
     </Layout>
   );
 };
 
-const GridItem = ({ children, ...props }) => <Box {...props}>{children}</Box>;
-
 export default CheckoutPage;
+
 
