430d55dabe565a60f28628ba2fa3c8e0676d2fad
saladinjake <juwavictor@gmail.com>
2022-05-15 02:00:00 +0100

    feat(auth): modernize signup page with premium immersive layout and refined form UX

--- pages/signup.js
+++ pages/signup.js
@@ -16,9 +16,11 @@ import {
   VStack,
   ScaleFade,
   SimpleGrid,
+  HStack,
+  Badge,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import { HiMail, HiLockClosed, HiUser } from "react-icons/hi";
+import { HiMail, HiLockClosed, HiUser, HiArrowRight, HiSparkles } from "react-icons/hi";
 import Link from "next/link";
 import Layout from "components/shared/blocks/Layout";
 import http from "utils/http";
@@ -42,7 +44,7 @@ const initialFieldsProps = [
   },
   {
     id: "email",
-    label: "Email",
+    label: "Email Address",
     placeholder: "john@example.com",
     type: "email",
     icon: HiMail,
@@ -73,15 +75,15 @@ const SignupPage = () => {
       await http.post("/auth/signup", body);
 
       toast.displayToast({
-        title: "Account created.",
-        description: "We've created your account for you.",
+        title: "Account created!",
+        description: "Welcome to the community. Please verify your email to get started.",
         status: "success",
       });
       router.push(`/signup/verify?email=${fieldsObj.email}`);
     } catch (err) {
       toast.displayToast({
-        title: "Error",
-        description: err.response?.data?.message || "Something went wrong",
+        title: "Sign up failed",
+        description: err.response?.data?.message || "Something went wrong. Please try again.",
         status: "error",
       });
     }
@@ -89,29 +91,44 @@ const SignupPage = () => {
 
   const form = useForm({ doSubmit, initialFieldsProps });
 
+  const bgColor = useColorModeValue("gray.50", "gray.900");
+  const cardBg = useColorModeValue("white", "gray.800");
+
   return (
     <Layout>
-      <Flex minH={"100vh"} align={"center"} justify={"center"} bg={useColorModeValue("gray.50", "gray.800")}>
-        <Stack spacing={0} mx={"auto"} maxW={"lg"} py={12} px={6} direction={{ base: "column", md: "row" }} w="full" rounded="3xl" overflow="hidden" shadow="2xl">
-
-          {/* Form Side */}
+      <Flex minH={"100vh"} align={"center"} justify={"center"} bg={bgColor} p={4}>
+        <Stack
+          spacing={0}
+          mx={"auto"}
+          maxW={"6xl"}
+          w="full"
+          direction={{ base: "column", md: "row" }}
+          rounded="3xl"
+          overflow="hidden"
+          shadow="2xl"
+          bg={cardBg}
+          border="1px solid"
+          borderColor={useColorModeValue("gray.100", "gray.700")}
+        >
+          {/* Left: Form */}
           <Box
             flex={1}
-            bg={useColorModeValue("white", "gray.700")}
-            p={{ base: 8, md: 12 }}
+            p={{ base: 8, md: 16 }}
             position="relative"
+            bg={cardBg}
             order={{ base: 2, md: 1 }}
           >
-            <ScaleFade initialScale={0.9} in={true}>
-              <Stack spacing={8}>
-                <Stack align={"center"}>
-                  <Heading fontSize={"3xl"} textAlign={"center"}>
-                    Create your account
+            <ScaleFade initialScale={0.95} in={true}>
+              <VStack spacing={8} align="stretch">
+                <VStack align="start" spacing={2}>
+                  <Heading size="xl" fontWeight="black" letterSpacing="tight">
+                    Join the Future
                   </Heading>
-                  <Text fontSize={"lg"} color={"gray.600"}>
-                    to start your journey with us 🚀
+                  <Text color={"gray.500"} fontSize="lg">
+                    Create your account to start your journey
                   </Text>
-                </Stack>
+                </VStack>
+
                 <Box
                   as="form"
                   onSubmit={(e) => {
@@ -119,25 +136,27 @@ const SignupPage = () => {
                     form.handleSubmit(e);
                   }}
                 >
-                  <Stack spacing={4}>
-                    <SimpleGrid columns={{ base: 1, sm: 2 }} spacing={4}>
+                  <Stack spacing={5}>
+                    <SimpleGrid columns={{ base: 1, sm: 2 }} spacing={5}>
                       {initialFieldsProps.slice(0, 2).map((field) => (
-                        <VStack align="start" key={field.id} spacing={1}>
-                          <Text fontWeight="medium" fontSize="sm">{field.label}</Text>
-                          <InputGroup>
+                        <VStack align="start" key={field.id} spacing={2}>
+                          <Text fontWeight="bold" fontSize="sm" color="gray.600">{field.label}</Text>
+                          <InputGroup size="lg">
                             <InputLeftElement pointerEvents="none">
-                              <Icon as={field.icon} color="gray.400" />
+                              <Icon as={field.icon} color="blue.400" />
                             </InputLeftElement>
                             <Input
                               type={field.type}
                               placeholder={field.placeholder}
-                              rounded="lg"
-                              bg={useColorModeValue("gray.50", "gray.800")}
-                              border={0}
+                              rounded="2xl"
+                              bg={useColorModeValue("gray.50", "gray.900")}
+                              borderWidth="2px"
+                              borderColor="transparent"
+                              fontSize="md"
                               _focus={{
-                                bg: "white",
-                                ring: 2,
-                                ringColor: "blue.400"
+                                bg: useColorModeValue("white", "gray.800"),
+                                borderColor: "blue.400",
+                                shadow: "lg",
                               }}
                               name={field.id}
                               onChange={form.handleType}
@@ -148,22 +167,24 @@ const SignupPage = () => {
                     </SimpleGrid>
 
                     {initialFieldsProps.slice(2).map((field) => (
-                      <VStack align="start" key={field.id} spacing={1}>
-                        <Text fontWeight="medium" fontSize="sm">{field.label}</Text>
-                        <InputGroup>
+                      <VStack align="start" key={field.id} spacing={2}>
+                        <Text fontWeight="bold" fontSize="sm" color="gray.600">{field.label}</Text>
+                        <InputGroup size="lg">
                           <InputLeftElement pointerEvents="none">
-                            <Icon as={field.icon} color="gray.400" />
+                            <Icon as={field.icon} color="blue.400" />
                           </InputLeftElement>
                           <Input
                             type={field.type}
                             placeholder={field.placeholder}
-                            rounded="lg"
-                            bg={useColorModeValue("gray.50", "gray.800")}
-                            border={0}
+                            rounded="2xl"
+                            bg={useColorModeValue("gray.50", "gray.900")}
+                            borderWidth="2px"
+                            borderColor="transparent"
+                            fontSize="md"
                             _focus={{
-                              bg: "white",
-                              ring: 2,
-                              ringColor: "blue.400"
+                              bg: useColorModeValue("white", "gray.800"),
+                              borderColor: "blue.400",
+                              shadow: "lg",
                             }}
                             name={field.id}
                             onChange={form.handleType}
@@ -172,72 +193,87 @@ const SignupPage = () => {
                       </VStack>
                     ))}
 
-                    <Stack spacing={10} pt={4}>
-                      <Button
-                        type="submit"
-                        bg={"blue.400"}
-                        color={"white"}
-                        rounded={"lg"}
-                        _hover={{
-                          bg: "blue.500",
-                          transform: "translateY(-2px)",
-                          boxShadow: "lg",
-                        }}
-                        _active={{
-                          bg: "blue.600",
-                        }}
-                        isLoading={form.formSubmitState.isSubmitting}
-                        transition="all 0.2s"
-                      >
-                        Sign up
-                      </Button>
-                    </Stack>
-                    <Stack pt={6}>
-                      <Text align={"center"}>
-                        Already a user?{" "}
+                    <Button
+                      type="submit"
+                      size="lg"
+                      colorScheme="blue"
+                      h={16}
+                      rounded="2xl"
+                      fontSize="lg"
+                      fontWeight="bold"
+                      shadow="xl"
+                      mt={4}
+                      rightIcon={<HiArrowRight />}
+                      _hover={{
+                        transform: "translateY(-2px)",
+                        shadow: "2xl",
+                      }}
+                      _active={{
+                        transform: "translateY(0)",
+                      }}
+                      isLoading={form.formSubmitState.isSubmitting}
+                      transition="all 0.2s"
+                    >
+                      Create Account
+                    </Button>
+
+                    <Stack pt={4}>
+                      <Text align={"center"} color="gray.500">
+                        Already have an account?{" "}
                         <Link href="/signin" passHref>
-                          <ChakraLink color={"blue.400"}>Login</ChakraLink>
+                          <ChakraLink color={"blue.500"} fontWeight="black">
+                            Log In
+                          </ChakraLink>
                         </Link>
                       </Text>
                     </Stack>
                   </Stack>
                 </Box>
-              </Stack>
+              </VStack>
             </ScaleFade>
           </Box>
 
-          {/* Image Side */}
+          {/* Right: Content */}
           <Box
             display={{ base: "none", md: "block" }}
-            flex={1}
-            bgImage="url('/img/auth-bg.png')"
+            flex={1.2}
+            bgImage="url('/img/herolanding.jpg')"
             bgSize="cover"
             bgPosition="center"
             position="relative"
             order={{ base: 1, md: 2 }}
           >
+            <Box position="absolute" inset={0} bgGradient="linear(to-br, blue.600, blue.900)" opacity={0.85} />
             <Flex
-              pos="absolute"
-              inset={0}
-              bg="blackAlpha.400"
+              pos="relative"
+              h="full"
               direction="column"
-              justify="center"
+              justify="space-between"
               p={12}
               color="white"
             >
-              <motion.div
-                initial={{ opacity: 0, x: 20 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.8 }}
-              >
-                <Heading size="2xl" mb={4}>Join the Community</Heading>
-                <Text fontSize="xl" opacity={0.9}>
-                  Create your account today and get access to exclusive features and premium tools.
+              <VStack align="start" spacing={2}>
+                <HStack color="blue.300">
+                  <Icon as={HiSparkles} />
+                  <Text fontWeight="black" letterSpacing="widest" fontSize="xs">JOIN THE REVOLUTION</Text>
+                </HStack>
+                <Heading size="3xl" fontWeight="black" letterSpacing="tight">
+                  The New Standard of <Text as="span" color="blue.300">Learning</Text>
+                </Heading>
+              </VStack>
+
+              <VStack align="start" spacing={6}>
+                <Text fontSize="xl" fontWeight="medium" opacity={0.9}>
+                  Access a global community of experts, premium learning paths, and powerful career-building tools.
                 </Text>
-              </motion.div>
+                <HStack spacing={4}>
+                  <Badge bg="whiteAlpha.300" color="white" px={3} py={1} rounded="full">Free Tier</Badge>
+                  <Badge bg="whiteAlpha.300" color="white" px={3} py={1} rounded="full">Networking</Badge>
+                  <Badge bg="whiteAlpha.300" color="white" px={3} py={1} rounded="full">Mentorship</Badge>
+                </HStack>
+              </VStack>
             </Flex>
           </Box>
-
         </Stack>
       </Flex>
     </Layout>
@@ -245,4 +281,5 @@ const SignupPage = () => {
 };
 
 export default SignupPage;
+
 
