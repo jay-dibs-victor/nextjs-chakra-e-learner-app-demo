623044ee6c2f0cbec643f4722806860e18bf8a7d
saladinjake <juwavictor@gmail.com>
2022-05-16 10:00:00 +0100

    feat(user): modernize wallet dashboard with transaction history and premium footer

--- core/views/components/shared/blocks/Footer.js
+++ core/views/components/shared/blocks/Footer.js
@@ -8,6 +8,11 @@ import {
   Icon,
   Stack,
   Text,
+  Input,
+  Button,
+  VStack,
+  Divider,
+  Container,
 } from "@chakra-ui/react";
 import {
   TiSocialFacebook,
@@ -15,91 +20,126 @@ import {
   TiSocialInstagram,
   TiSocialYoutube,
 } from "react-icons/ti";
+import { 
+  HiMail, 
+  HiPhone, 
+  HiLocationMarker, 
+  HiArrowRight 
+} from "react-icons/hi";
 import Brand from "./Brand";
 import { Link } from "./Link";
-import { Container } from "./Layout";
 
 const Footer = () => {
   return (
-    <Box bg="blue.900" color="gray.200" pt={16} pb={8}>
-      <Container>
+    <Box bg="blue.900" color="gray.200" pt={24} pb={12} position="relative" overflow="hidden">
+      {/* Background Decor */}
+      <Box position="absolute" top="-10%" left="-10%" w="40%" h="140%" bg="blue.800" opacity={0.3} rounded="full" blur="100px" />
+      
+      <Container maxW="container.xl" position="relative" zIndex={1}>
         <Grid
           templateColumns={{
             base: "1fr",
-            md: "1fr 1fr",
-            lg: "2fr 1fr 1fr 1.5fr",
+            lg: "1.5fr 2fr",
           }}
-          gap={10}
-          mb={12}
+          gap={20}
+          mb={20}
         >
-          {/* Brand & Socials */}
-          <GridItem>
-            <Box maxW="300px">
-              <Brand size="2xl" color="white" mb={4} />
-              <Text fontSize="sm" color="gray.400" mb={6} lineHeight="tall">
-                Empowering the modern workforce with AI-driven learning and recruitment solutions.
+          {/* Left Side: Brand & Newsletter */}
+          <VStack align="start" spacing={10}>
+            <VStack align="start" spacing={6}>
+              <Brand size="3xl" color="white" />
+              <Text fontSize="lg" color="blue.100" opacity={0.8} maxW="400px" lineHeight="tall">
+                Revolutionizing the future of work and learning with AI-powered ecosystems that empower professionals globally.
               </Text>
+            </VStack>
 
-              <HStack spacing={4}>
-                <SocialLink icon={TiSocialFacebook} />
-                <SocialLink icon={TiSocialTwitter} />
-                <SocialLink icon={TiSocialInstagram} />
-                <SocialLink icon={TiSocialYoutube} />
+            <VStack align="start" spacing={4} w="full" maxW="450px">
+              <Heading size="sm" color="white" textTransform="uppercase" letterSpacing="widest">Join our Newsletter</Heading>
+              <Text fontSize="sm" color="blue.200">Get the latest updates on career opportunities and tech trends.</Text>
+              <HStack w="full" bg="whiteAlpha.100" p={1} rounded="full" border="1px solid" borderColor="whiteAlpha.200">
+                <Input 
+                  placeholder="Enter your email" 
+                  variant="unstyled" 
+                  px={6} 
+                  fontSize="sm" 
+                  color="white"
+                  _placeholder={{ color: "whiteAlpha.500" }}
+                />
+                <Button colorScheme="blue" rounded="full" px={8} h={12} rightIcon={<HiArrowRight />}>
+                  Subscribe
+                </Button>
               </HStack>
-            </Box>
-          </GridItem>
+            </VStack>
+          </VStack>
 
-          {/* Quick Links */}
-          <GridItem>
-            <Heading as="h6" size="sm" color="white" textTransform="uppercase" letterSpacing="wider" mb={6}>
-              Company
-            </Heading>
-            <Stack spacing={3}>
-              <FooterLink>About Us</FooterLink>
-              <FooterLink>Careers</FooterLink>
-              <FooterLink>Our Programs</FooterLink>
-              <FooterLink>Contact</FooterLink>
-            </Stack>
-          </GridItem>
+          {/* Right Side: Links Grid */}
+          <SimpleGrid columns={{ base: 2, md: 3 }} spacing={10} w="full">
+            <VStack align="start" spacing={6}>
+               <Heading size="xs" color="white" textTransform="uppercase" letterSpacing="widest">Company</Heading>
+               <Stack spacing={4}>
+                  <FooterLink href="/about">About Ace-Trace</FooterLink>
+                  <FooterLink href="/careers">Careers</FooterLink>
+                  <FooterLink href="/workforce">Workforce</FooterLink>
+                  <FooterLink href="/contact">Contact Us</FooterLink>
+               </Stack>
+            </VStack>
 
-          {/* Resources */}
-          <GridItem>
-            <Heading as="h6" size="sm" color="white" textTransform="uppercase" letterSpacing="wider" mb={6}>
-              Resources
-            </Heading>
-            <Stack spacing={3}>
-              <FooterLink>Help Center</FooterLink>
-              <FooterLink>Success Stories</FooterLink>
-              <FooterLink>API Documentation</FooterLink>
-              <FooterLink>Blog</FooterLink>
-            </Stack>
-          </GridItem>
+            <VStack align="start" spacing={6}>
+               <Heading size="xs" color="white" textTransform="uppercase" letterSpacing="widest">Solutions</Heading>
+               <Stack spacing={4}>
+                  <FooterLink href="/elearning-accelerator">E-Learning</FooterLink>
+                  <FooterLink href="/store">Courses</FooterLink>
+                  <FooterLink href="/talent-sourcing">Talent Sourcing</FooterLink>
+                  <FooterLink href="/accelerator">Startup Hub</FooterLink>
+               </Stack>
+            </VStack>
 
-          {/* Legal */}
-          <GridItem>
-            <Heading as="h6" size="sm" color="white" textTransform="uppercase" letterSpacing="wider" mb={6}>
-              Legal
-            </Heading>
-            <Stack spacing={3}>
-              <FooterLink>Terms of Service</FooterLink>
-              <FooterLink>Privacy Policy</FooterLink>
-              <FooterLink>Cookie Policy</FooterLink>
-            </Stack>
-          </GridItem>
+            <VStack align="start" spacing={6}>
+               <Heading size="xs" color="white" textTransform="uppercase" letterSpacing="widest">Support</Heading>
+               <Stack spacing={4}>
+                  <HStack spacing={3} color="blue.200">
+                     <Icon as={HiMail} />
+                     <Text fontSize="sm">support@ace-trace.tech</Text>
+                  </HStack>
+                  <HStack spacing={3} color="blue.200">
+                     <Icon as={HiPhone} />
+                     <Text fontSize="sm">+234 (0) 800-IMPACT</Text>
+                  </HStack>
+                  <HStack spacing={3} color="blue.200">
+                     <Icon as={HiLocationMarker} />
+                     <Text fontSize="sm">Lekki, Lagos, Nigeria</Text>
+                  </HStack>
+               </Stack>
+            </VStack>
+          </SimpleGrid>
         </Grid>
 
-        {/* Copyright */}
+        <Divider borderColor="whiteAlpha.200" mb={12} />
+
+        {/* Footer Bottom */}
         <Flex
           direction={{ base: "column", md: "row" }}
           justify="space-between"
           align="center"
-          borderTop="1px solid"
-          borderColor="whiteAlpha.200"
-          pt={8}
+          gap={8}
         >
-          <Text fontSize="sm" color="gray.500" mb={{ base: 4, md: 0 }}>
-            © {new Date().getFullYear()} Ace-Trace LLC. All rights reserved.
-          </Text>
+          <VStack align={{ base: "center", md: "start" }} spacing={1}>
+             <Text fontSize="sm" color="whiteAlpha.600">
+               © {new Date().getFullYear()} ImpactXplorer by Ace-Trace. All rights reserved.
+             </Text>
+             <HStack spacing={6} fontSize="xs" color="whiteAlpha.400" fontWeight="bold" textTransform="uppercase">
+                <Link mute href="/privacy">Privacy Policy</Link>
+                <Link mute href="/terms">Terms of Service</Link>
+                <Link mute href="/cookies">Cookie Settings</Link>
+             </HStack>
+          </VStack>
+
+          <HStack spacing={4}>
+            <SocialLink icon={TiSocialFacebook} />
+            <SocialLink icon={TiSocialTwitter} />
+            <SocialLink icon={TiSocialInstagram} />
+            <SocialLink icon={TiSocialYoutube} />
+          </HStack>
         </Flex>
       </Container>
     </Box>
@@ -108,22 +148,37 @@ const Footer = () => {
 
 const SocialLink = ({ icon }) => (
   <Link href="#" isExternal>
-    <Flex
-      align="center" justify="center"
-      w={10} h={10}
-      rounded="full" bg="whiteAlpha.100" color="white"
-      transition="all 0.3s"
-      _hover={{ bg: "blue.500", color: "white", transform: "translateY(-2px)" }}
+    <Circle
+      size={12}
+      bg="whiteAlpha.100"
+      color="white"
+      transition="all 0.3s cubic-bezier(.175,.885,.32,1.275)"
+      _hover={{ bg: "blue.500", transform: "scale(1.1) translateY(-4px)", shadow: "0 10px 20px rgba(0,0,0,0.2)" }}
     >
-      <Icon as={icon} boxSize={5} />
-    </Flex>
+      <Icon as={icon} boxSize={6} />
+    </Circle>
   </Link>
 );
 
-const FooterLink = ({ children }) => (
-  <Link mute fontSize="sm" color="gray.400" _hover={{ color: "white", textDecoration: "none" }} transition="color 0.2s">
+const FooterLink = ({ children, href }) => (
+  <Link 
+    href={href} 
+    mute 
+    fontSize="sm" 
+    color="blue.100" 
+    opacity={0.7} 
+    _hover={{ opacity: 1, color: "white", transform: "translateX(4px)" }} 
+    transition="all 0.2s"
+  >
     {children}
   </Link>
 );
 
+const SimpleGrid = ({ children, columns, ...rest }) => (
+  <Grid templateColumns={{ base: "1fr", md: `repeat(${columns.md || columns.base}, 1fr)` }} {...rest}>
+    {children}
+  </Grid>
+);
+
 export default Footer;
+
--- pages/users/wallet.js
+++ pages/users/wallet.js
@@ -1,256 +1,228 @@
 
 
-import { ButtonGroup } from "@chakra-ui/button";
-import { Box, Flex, Grid, GridItem } from "@chakra-ui/layout";
-import { Button, Heading, Icon, Text, Modal, TextField } from "components/shared/lib";
-import { Layout, PageHeader, Section } from "components/components/pages";
-import useForm from "hooks/useForm";
-import { FaPen, FaTrashAlt } from "react-icons/fa";
-import { IoAdd } from "react-icons/io5";
-import breakpoints from "theme/breakpoints";
+import React from "react";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Text,
+  Icon,
+  HStack,
+  VStack,
+  Divider,
+  Badge,
+  Circle,
+  useColorModeValue,
+  SimpleGrid,
+  Container,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react";
+import { 
+  HiCreditCard, 
+  HiArrowUp, 
+  HiArrowDown, 
+  HiPlus, 
+  HiLightningBolt, 
+  HiShieldCheck,
+  HiClock
+} from "react-icons/hi";
+import { motion } from "framer-motion";
+import { Button, Modal, TextField } from "components/shared/lib";
+import { Layout, Section } from "components/components/pages";
 import buildSEO from "utils/buildSEO";
-import http from "utils/http";
+import formatPrice from "utils/formatPrice";
 
-const pageSEO = buildSEO("My Preferences", "...");
+const pageSEO = buildSEO("My Wallet", "Manage your funds, view transactions, and link payment methods");
 
-const PopModal = ({
-  heading,
-  initialFieldsProps,
-  doSubmit,
-  renderTrigger,
-  submitButtonText,
-}) => {
-  const { formProps, handleType, fieldsProps, renderSubmitBtn } = useForm({
-    doSubmit,
-    initialFieldsProps,
-  });
+const MotionBox = motion(Box);
 
-  return (
-    <Modal renderTrigger={renderTrigger}>
-      {({ handleClose }) => (
-        <Box as="form" {...formProps}>
-          <Box as="header">
-            <Heading type="h5">{heading}</Heading>
-          </Box>
-
-          {fieldsProps.map((field) => (
-            <TextField {...field} key={field.id} onChange={handleType} />
-          ))}
-
-          <Flex justifyContent="flex-end">
-            <ButtonGroup spacing={2} justifyContent="flex-end">
-              {renderSubmitBtn({
-                text: submitButtonText || "Add",
-                variant: "primary",
-                sm: true,
-              })}
-
-              <Button variant="secondary" sm onClick={handleClose}>
-                Cancel
-              </Button>
-            </ButtonGroup>
-          </Flex>
-        </Box>
-      )}
-    </Modal>
-  );
-};
-
-const Card = ({ heading, data, subHeading, onEdit }) => {
-  const handleRemove = async () => {
-    const yes = confirm("Are you sure you want to REMOVE this?");
-
-    if (yes) {
-      // Make a request to remove this card info...
-      await http.get("/me");
-
-      // cause a reload
-      location.reload();
-    }
-  };
-
-  const renderControl = (onClick) => (
-    <Flex
-      alignItems="center"
-      justifyContent="space-between"
-      color={onClick ? "brand.black2" : "brand.error"}
-      _hover={{
-        bg: "brand.gray5",
-        cursor: "pointer",
-      }}
-      onClick={onClick || handleRemove}
-      rounded="md"
-      px={2}
-    >
-      <Text mute type={onClick ? "nm-bold" : "nm-regular"}>
-        {onClick ? "Edit" : "Remove"}
-      </Text>
-
-      <Icon fontSize="70%" ml={2}>
-        {onClick ? <FaPen /> : <FaTrashAlt />}
-      </Icon>
-    </Flex>
-  );
-
-  const renderRemoveButton = () => renderControl();
-
-  const renderEditButton = () => (
-    <PopModal
-      heading={subHeading}
-      initialFieldsProps={data}
-      doSubmit={onEdit}
-      renderTrigger={({ handleOpen }) => renderControl(handleOpen)}
-      submitButtonText="Edit"
-    />
-  );
-
-  return (
-    <Box>
-      <Text type="md-bold" mb={2}>
-        {heading}
-      </Text>
-
-      <Flex justifyContent="space-between" bg="brand.gray5" rounded="md" p={3}>
-        <Grid templateColumns="1fr 1fr 1fr" columnGap={5}>
-          {data.map((item, index) => (
-            <GridItem key={index}>
-              <Text type="nm-bold" mute>
-                {item.label}
-              </Text>
-              <Text>{item.value}</Text>
-            </GridItem>
-          ))}
-        </Grid>
-
-        <Box alignSelf="flex-end">
-          {renderEditButton()}
-          {renderRemoveButton()}
-        </Box>
-      </Flex>
-    </Box>
-  );
-};
-
-const Header = ({ initialFieldsProps, heading, subHeading, onAdd }) => (
-  <Flex
-    alignItems="center"
-    justifyContent="space-between"
-    borderBottom="1px"
-    color="brand.secondary"
-    mb={5}
-    pb={2}
+const BalanceCard = ({ label, amount, icon, color }) => (
+  <MotionBox
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    bg="white"
+    p={8}
+    rounded="3xl"
+    shadow="xl"
+    borderWidth="1px"
+    borderColor="gray.50"
+    position="relative"
+    overflow="hidden"
   >
-    <Heading type="h5" mute>
-      {heading}
-    </Heading>
-
-    <PopModal
-      heading={subHeading}
-      initialFieldsProps={initialFieldsProps}
-      doSubmit={onAdd}
-      renderTrigger={({ handleOpen }) => (
-        <Button
-          leftIcon={<IoAdd />}
-          variant="secondary"
-          sm
-          onClick={handleOpen}
-        >
-          Add
-        </Button>
-      )}
-    />
-  </Flex>
+    <Circle size={12} bg={`${color}.50`} color={`${color}.500`} mb={4}>
+       <Icon as={icon} w={6} h={6} />
+    </Circle>
+    <Stat>
+      <StatLabel color="gray.500" fontWeight="bold" textTransform="uppercase" fontSize="xs" letterSpacing="widest">{label}</StatLabel>
+      <StatNumber fontSize="3xl" fontWeight="black" color="blue.900">{formatPrice("en-NG", amount, "NGN")}</StatNumber>
+      <StatHelpText color="green.500" fontWeight="bold">
+        <Icon as={HiArrowUp} mr={1} /> 12% from last month
+      </StatHelpText>
+    </Stat>
+    <Box position="absolute" bottom="-10%" right="-10%" opacity={0.05}>
+       <Icon as={icon} w="150px" h="150px" color="blue.900" />
+    </Box>
+  </MotionBox>
 );
 
-const EditPreferencePage = () => {
-  const formDataInitializer = (formData) =>
-    formData.map((item) => ({ ...item, value: "" }));
+const TransactionRow = ({ type, description, date, amount, status }) => (
+  <Tr _hover={{ bg: "gray.50" }} transition="background 0.2s">
+    <Td>
+      <HStack spacing={4}>
+        <Circle size={10} bg={type === "credit" ? "green.50" : "red.50"} color={type === "credit" ? "green.500" : "red.500"}>
+          <Icon as={type === "credit" ? HiArrowDown : HiArrowUp} />
+        </Circle>
+        <VStack align="start" spacing={0}>
+          <Text fontWeight="black" fontSize="sm">{description}</Text>
+          <Text fontSize="xs" color="gray.400">{date}</Text>
+        </VStack>
+      </HStack>
+    </Td>
+    <Td>
+       <Badge colorScheme={status === "completed" ? "green" : "orange"} variant="subtle" rounded="full" px={3}>
+          {status}
+       </Badge>
+    </Td>
+    <Td isNumeric>
+      <Text fontWeight="black" color={type === "credit" ? "green.600" : "red.600"}>
+        {type === "credit" ? "+" : "-"}{formatPrice("en-NG", amount, "NGN")}
+      </Text>
+    </Td>
+  </Tr>
+);
 
-  const accountInfoCardData = [
-    {
-      id: "address",
-      label: "Address",
-      textarea: true,
-      minH: "40px",
-      value: "85 Ayilara street, ojuelegba, Lagos state, Nigeria",
-    },
-    {
-      id: "phone-number",
-      label: "Phone Number",
-      type: "number",
-      value: "07032917504",
-    },
+const WalletPage = () => {
+  const transactions = [
+    { type: "debit", description: "UI Design Course Enrollment", date: "May 14, 2022", amount: 25000, status: "completed" },
+    { type: "credit", description: "Wallet Funding via Paystack", date: "May 12, 2022", amount: 50000, status: "completed" },
+    { type: "debit", description: "E-learning Accelerator Monthly Subscription", date: "May 10, 2022", amount: 15000, status: "completed" },
+    { type: "credit", description: "Referral Bonus", date: "May 05, 2022", amount: 5000, status: "completed" },
   ];
-
-  const cardDetailsCardData = [
-    {
-      id: "card-name",
-      label: "Card Name",
-      value: "Salau Oluwole Olorunjuedalo",
-    },
-    {
-      id: "card-type",
-      label: "Card Type",
-      value: "VISA",
-    },
-    {
-      id: "card-number",
-      label: "Card Number",
-      value: "53999 xxxx",
-    },
-  ];
-
-  const accountInfoFormData = formDataInitializer(accountInfoCardData);
-  const cardDetailsFormData = formDataInitializer(cardDetailsCardData);
-
-  const handleAddAccountInfo = async (fieldsObj, router) => {
-    console.log(fieldsObj);
-    await http.get("/me");
-
-    router.reload();
-  };
-  const handleAddCardDetail = async (fieldsObj, router) => {
-    console.log(fieldsObj);
-    await http.get("/me");
-
-    router.reload();
-  };
 
   return (
     <Layout SEO={pageSEO}>
-      {/* <PageHeader>Edit Preference</PageHeader> */}
+      <Box bg="gray.50" minH="100vh" pt={12} pb={24}>
+        <Container maxW="container.xl">
+          <VStack spacing={12} align="stretch">
+            {/* Header Section */}
+            <Flex justify="space-between" align="end" direction={{ base: "column", md: "row" }} gap={6}>
+              <VStack align="start" spacing={2}>
+                <HStack color="blue.500">
+                  <Icon as={HiCreditCard} w={6} h={6} />
+                  <Text fontWeight="black" letterSpacing="widest" fontSize="xs">MY WALLET</Text>
+                </HStack>
+                <Heading size="2xl" fontWeight="black" letterSpacing="tight">Financial Hub</Heading>
+              </VStack>
+              
+              <HStack spacing={4}>
+                 <Button colorScheme="blue" rounded="full" h={14} px={8} leftIcon={<HiPlus />} shadow="xl" _hover={{ transform: "translateY(-2px)", shadow: "2xl" }}>
+                    Add Funds
+                 </Button>
+                 <Button variant="ghost" colorScheme="blue" rounded="full" h={14} px={8} leftIcon={<HiArrowUp />}>
+                    Withdraw
+                 </Button>
+              </HStack>
+            </Flex>
 
-      <Section mb={16} maxW={breakpoints.md}>
-        <Header
-          heading="Addresses"
-          subHeading="Account Information"
-          onAdd={handleAddAccountInfo}
-          initialFieldsProps={accountInfoFormData}
-        />
-        <Card
-          heading="Account Information"
-          data={accountInfoCardData}
-          subHeading="Account Information"
-          onEdit={handleAddAccountInfo}
-        />
-      </Section>
+            {/* Balance Overview */}
+            <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8}>
+              <BalanceCard label="Current Balance" amount={120500} icon={HiLightningBolt} color="blue" />
+              <BalanceCard label="Total Spent" amount={450000} icon={HiArrowUp} color="orange" />
+              <BalanceCard label="Pending Rewards" amount={15000} icon={HiClock} color="purple" />
+            </SimpleGrid>
 
-      <Section mb={16} maxW={breakpoints.md}>
-        <Header
-          heading="Payments"
-          subHeading="Card Details"
-          onAdd={handleAddCardDetail}
-          initialFieldsProps={cardDetailsFormData}
-        />
-        <Card
-          heading="Card Details"
-          data={cardDetailsCardData}
-          subHeading="Card Details"
-          onEdit={handleAddCardDetail}
-        />
-      </Section>
+            {/* Transaction History */}
+            <Box bg="white" rounded="3xl" shadow="xl" p={8} borderWidth="1px" borderColor="gray.100">
+               <Flex justify="space-between" align="center" mb={8}>
+                  <Heading size="md" fontWeight="black">Recent Activity</Heading>
+                  <Button variant="link" colorScheme="blue" size="sm">View All Transactions</Button>
+               </Flex>
+               
+               <Box overflowX="auto">
+                  <Table variant="simple">
+                    <Thead>
+                      <Tr>
+                        <Th fontSize="xs" letterSpacing="widest">Transaction</Th>
+                        <Th fontSize="xs" letterSpacing="widest">Status</Th>
+                        <Th fontSize="xs" letterSpacing="widest" isNumeric>Amount</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      {transactions.map((tx, idx) => (
+                        <TransactionRow key={idx} {...tx} />
+                      ))}
+                    </Tbody>
+                  </Table>
+               </Box>
+            </Box>
+
+            {/* Link New Card Section */}
+            <Box bg="blue.900" rounded="3xl" p={12} color="white" position="relative" overflow="hidden">
+               <Box position="absolute" top="-20%" right="-10%" w="40%" h="140%" bg="whiteAlpha.100" rounded="full" blur="40px" />
+               <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={12} alignItems="center" position="relative" zIndex={1}>
+                  <VStack align="start" spacing={6}>
+                     <HStack color="blue.300">
+                        <Icon as={HiShieldCheck} />
+                        <Text fontWeight="bold" fontSize="sm">SECURE PAYMENTS</Text>
+                     </HStack>
+                     <Heading size="xl" fontWeight="black" lineHeight="tight">Link your card for instant funding</Heading>
+                     <Text fontSize="lg" opacity={0.8}>
+                        Securely save your payment methods to enable one-click funding and faster checkout across the platform.
+                     </Text>
+                     <Button bg="white" color="blue.900" rounded="full" px={10} h={14} fontWeight="black" _hover={{ bg: "blue.50", transform: "scale(1.05)" }}>
+                        Link New Card
+                     </Button>
+                  </VStack>
+                  <Center>
+                     <Box 
+                        bgGradient="linear(to-br, blue.400, blue.600)" 
+                        w="full" 
+                        maxW="400px" 
+                        h="240px" 
+                        rounded="2xl" 
+                        p={8} 
+                        shadow="2xl"
+                        position="relative"
+                     >
+                        <HStack justify="space-between" align="start">
+                           <Icon as={HiCreditCard} w={12} h={12} color="whiteAlpha.800" />
+                           <Text fontWeight="black" letterSpacing="widest">PREMIUM</Text>
+                        </HStack>
+                        <VStack align="start" mt={8} spacing={1}>
+                           <Text fontSize="xs" opacity={0.6} textTransform="uppercase">Card Number</Text>
+                           <Text fontSize="xl" letterSpacing="widest" fontWeight="bold">**** **** **** 5399</Text>
+                        </VStack>
+                        <HStack justify="space-between" mt={4}>
+                           <VStack align="start" spacing={0}>
+                              <Text fontSize="2xs" opacity={0.6} textTransform="uppercase">Expiry</Text>
+                              <Text fontSize="sm" fontWeight="bold">12/25</Text>
+                           </VStack>
+                           <VStack align="end" spacing={0}>
+                              <Text fontSize="2xs" opacity={0.6} textTransform="uppercase">Card Holder</Text>
+                              <Text fontSize="sm" fontWeight="bold">JOHN DOE</Text>
+                           </VStack>
+                        </HStack>
+                     </Box>
+                  </Center>
+               </SimpleGrid>
+            </Box>
+          </VStack>
+        </Container>
+      </Box>
     </Layout>
   );
 };
 
-export default EditPreferencePage;
+export default WalletPage;
+
