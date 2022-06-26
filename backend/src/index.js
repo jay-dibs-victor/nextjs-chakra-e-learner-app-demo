2391f3fb6010ee9bab94ce79c1dad11e7285105b
saladinjake <juwavictor@gmail.com>
2022-06-26 17:05:00 +0100

    Fix: CORS policy to allow credentials and specific frontend origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // MongoDB Connection
